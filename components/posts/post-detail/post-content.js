001051523e299247036c4d5d3e116da67913efe4
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-01-26 21:43:08 +0000

    rendering markdown as jsx

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,7 @@
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+import ReactMarkdown from "react-markdown";
+
 function PostContent(props) {
   const { image, slug, title, content } = props.post;
   const imagePath = `/images/posts/${slug}/${image}`;
@@ -7,6 +9,7 @@ function PostContent(props) {
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
+      <ReactMarkdown>{content}</ReactMarkdown>
     </article>
   );
 }
