daeb19dbbc654417b9e5bff539efafcfb68af34e
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-01-26 21:10:50 +0000

    adding layout and navigation

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react/cjs/react.production.min";
+import MainNaivation from "./main-navigation";
+function Layout(props) {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main>{props.children}</main>
+    </Fragment>
+  );
+}
+
+export default Layout;
---
+++ components/layout/logo.js
@@ -0,0 +1,7 @@
+import { Fragment } from "react/cjs/react.production.min";
+import classes from "./logo.module.css";
+function Logo(props) {
+  return <div className={classes.logo}>Next Blog</div>;
+}
+
+export default Logo;
