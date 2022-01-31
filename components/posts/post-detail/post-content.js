a85027859cea2116f0a2430e9a25d794e1c212d0
Javier.Pesado <javier.pesado@insurancerevolution.co.uk>
2022-01-31 21:25:05 +0000

    rendering images with the next image component inside react markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,45 @@
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
-
+import Image from "next/image";
 function PostContent(props) {
   const { image, slug, title, content } = props.post;
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  const customRenderes = {
+    //react markdown renders images as paragrahs
+    /* img(image) {
+      return (
+        <Image
+          src={`/images/posts/${post.src}/${image.url}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },*/
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderes}>{content}</ReactMarkdown>
     </article>
   );
 }
